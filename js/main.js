20cab2499ead141105e3adccfff7d02d65e13d78
MASHA <mashanikit@gmail.com>
2017-07-06 16:23:04 -0400

    finished

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 
+// When someone hits the button in the switch, remove the current state class (on/off), and apply the opposite class (on/off)
 
+document.addEventListener('DOMContentLoaded', function() {
+  var lightSwitch = document.querySelector('button');
+  var body = document.querySelector('body');
+  var status = document.querySelector('.status');
+
+  lightSwitch.addEventListener('click', function(e) {
+    if (lightSwitch.className === 'switch on') {
+      lightSwitch.className = 'switch off';
+      body.className = 'dark';
+      status.innerHTML = "Hey, who turned off the lights?";
+    } else {
+      lightSwitch.className = 'switch on';
+      body.className = 'light';
+      status.innerHTML = "It's so bright in here!"
+    }
+  });
+});
